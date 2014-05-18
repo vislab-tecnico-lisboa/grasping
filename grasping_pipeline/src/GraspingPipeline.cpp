01c860db64abb7c8b9b7c840b5f6a0df68ca54e5
rui <rui.defigueiredo@kustar.ac.ae>
2014-05-18 18:08:10 +0400

    done

--- grasping_pipeline/src/GraspingPipeline.cpp
+++ grasping_pipeline/src/GraspingPipeline.cpp
@@ -1,1 +1,11 @@
 #include <grasping_pipeline/GraspingPipeline.h>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "grasping_pipeline_server");
+
+    GraspingPipelineAction grasping_pipeline(ros::this_node::getName());
+    ros::spin();
+
+    return 0;
+}
