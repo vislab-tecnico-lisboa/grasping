fd2f5e598f65213b8963b474501acf8703d96685
rui <ruihortafigueiredo@gmail.com>
2016-12-12 01:05:02 +0000

    changed grasping frame related parameters to vizzy right hand

--- grasp_planning/scripts/move_group_test.py
+++ grasp_planning/scripts/move_group_test.py
@@ -73,7 +73,7 @@ def move_group_python_interface_tutorial():
   ## to one group of joints.  In this case the group is the joints in the left
   ## arm.  This interface can be used to plan and execute motions on the left
   ## arm.
-  group = moveit_commander.MoveGroupCommander("mico_arm_group")
+  group = moveit_commander.MoveGroupCommander("right_arm")
 
 
   ## We create this DisplayTrajectory publisher which is used below to publish
